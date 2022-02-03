5446f61e0d1a9f8f42edcaa2778aea9ca7221b39
Sebastien_Verwerft <sebastien.verwerft@gmail.com>
2022-02-03 16:19:05 +0100

    Modifications of prefabs, trick some little variables

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,13 +6,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _movingSpeed;
     [SerializeField] private float _sideStepSpeed;
 
+    [Header("UI ref")]
+    [SerializeField] private GameObject _texts;
+
+
     private float _timeElapsed;
     private Vector3 _moveDirection;
+
+    private Animator _animator;
 
     void Start()
     {
         // Init
         _moveDirection = Vector3.forward;
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,9 +27,17 @@ public class PlayerController : MonoBehaviour
         // Detect if we touch the screen
         if(Input.touchCount > 0)
         {
+            // Remove the intro text to start and start running
+            if(_texts.activeSelf)
+            {
+                _animator.SetTrigger("Run");
+                _texts.SetActive(false);
+            }
+
             // Get the first input only
             Touch touch = Input.GetTouch(0);
 
+            // Reset the _time for the lerp
             if(touch.phase == TouchPhase.Began)
             {
                 _timeElapsed = 0f;
@@ -58,7 +73,10 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Moving the character
-        transform.Translate(_moveDirection * Time.deltaTime * _movingSpeed);
+        if(!_texts.activeSelf)
+        {
+            // Moving the character
+            transform.Translate(_moveDirection * Time.deltaTime * _movingSpeed);
+        }
     }
 }
